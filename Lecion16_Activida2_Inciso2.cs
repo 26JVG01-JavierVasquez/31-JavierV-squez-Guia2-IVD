d18fc39c65336f0d7e2bd7ffa85a74ed6f83474f
26JVG01-JavierVasquez <vasquezjavier16db@gmail.com>
2026-06-18 17:09:30 -0600

    Add files via upload

---
+++ Lecion16_Activida2_Inciso2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numerosEnteros = new int[8];
+        int numeroMayor = 0;
+        int posicionMayor = 0;
+
+        for (int indice = 0; indice < 8; indice++)
+        {
+            Console.Write("Ingrese el número " + (indice + 1) + ": ");
+            numerosEnteros[indice] = Convert.ToInt32(Console.ReadLine());
+
+            if (indice == 0)
+            {
+                numeroMayor = numerosEnteros[indice];
+                posicionMayor = indice + 1;
+            }
+            else if (numerosEnteros[indice] > numeroMayor)
+            {
+                numeroMayor = numerosEnteros[indice];
+                posicionMayor = indice + 1;
+            }
+        }
+
+        Console.WriteLine("El número mayor es: " + numeroMayor);
+        Console.WriteLine("Se encuentra en la posición: " + posicionMayor);
+    }
+}

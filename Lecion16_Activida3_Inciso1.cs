eba5feac5d88286cd34c774b551497828e9eb006
26JVG01-JavierVasquez <vasquezjavier16db@gmail.com>
2026-06-18 17:08:59 -0600

    Add files via upload

---
+++ Lecion16_Activida3_Inciso1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numerosEnteros = new int[15];
+        bool existeRepetido = false;
+
+        for (int indice = 0; indice < 15; indice++)
+        {
+            Console.Write("Ingrese el número " + (indice + 1) + ": ");
+            numerosEnteros[indice] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (int indice = 0; indice < 14; indice++)
+        {
+            for (int comparador = indice + 1; comparador < 15; comparador++)
+            {
+                if (numerosEnteros[indice] == numerosEnteros[comparador])
+                {
+                    existeRepetido = true;
+                }
+            }
+        }
+
+        if (existeRepetido)
+        {
+            Console.WriteLine("Sí existe al menos un número repetido.");
+        }
+        else
+        {
+            Console.WriteLine("No existe ningún número repetido.");
+        }
+    }
+}

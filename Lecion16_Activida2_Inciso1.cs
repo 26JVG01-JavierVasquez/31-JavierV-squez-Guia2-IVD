d4b847f14bd2decb7fa1ccdf771b9bc96d0cdb78
26JVG01-JavierVasquez <vasquezjavier16db@gmail.com>
2026-06-18 17:10:02 -0600

    Add files via upload

---
+++ Lecion16_Activida2_Inciso1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] edadesPersonas = new int[7];
+        int cantidadMayoresEdad = 0;
+
+        for (int indice = 0; indice < 7; indice++)
+        {
+            Console.Write("Ingrese la edad de la persona " + (indice + 1) + ": ");
+            edadesPersonas[indice] = Convert.ToInt32(Console.ReadLine());
+
+            if (edadesPersonas[indice] >= 18)
+            {
+                cantidadMayoresEdad++;
+            }
+        }
+
+        Console.WriteLine("Cantidad de personas mayores de edad: " + cantidadMayoresEdad);
+    }
+}
